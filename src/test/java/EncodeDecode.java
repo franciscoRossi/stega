e615a39565f8f5015a8ee623c5e47dc0675cacdc
pupi8888 <pupi_8888@hotmail.com>
2019-07-17 19:46:30 -0300

    Fixing test

--- src/test/java/EncodeDecode.java
+++ src/test/java/EncodeDecode.java
@@ -3,33 +3,35 @@ import org.junit.Test;
 import utils.AES;
 import utils.Steganography;
 
+import java.security.GeneralSecurityException;
+
 /**
  * Created by pupi on 16/07/19.
  */
 public class EncodeDecode {
 
     @Test
-    public void testEncodeAndDecode() {
-//        Steganography stega = new Steganography();
-//        AES aes = new AES();
-//
-//        Message textInput = new Message("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
-//        String passphrase = "supersecretpasssword!";
-//
-//        //encrypt the string
-//        String encryptedText = aes.encrypt(textInput.toString(), passphrase);
-//
-//        //steganography encode
-//        String resourcePath = this.getClass().getResource("").getFile();
-//        stega.encode(resourcePath, "bliss", "jpg", "out", encryptedText);
-//
-//        //steganography decode
-//        String decodedImageContent = stega.decode(resourcePath, "out");
-//
-//        //decrypt
-//        String decryptedText = aes.decrypt(decodedImageContent, passphrase);
-//
-//        System.out.println(decryptedText);
-//        assert decryptedText.equals(textInput.toString());
+    public void testEncodeAndDecode() throws GeneralSecurityException {
+        Steganography stega = new Steganography();
+        AES aes = new AES();
+
+        Message textInput = new Message("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+        String passphrase = "supersecretpasssword!";
+
+        //encrypt the string
+        String encryptedText = aes.encrypt(textInput.toString(), passphrase);
+
+        //steganography encode
+        String resourcePath = this.getClass().getResource("").getFile();
+        stega.encode(resourcePath + "bliss.jpg", resourcePath + "out.png", encryptedText);
+
+        //steganography decode
+        String decodedImageContent = stega.decode(resourcePath + "out.png");
+
+        //decrypt
+        String decryptedText = aes.decrypt(decodedImageContent, passphrase);
+
+        System.out.println(decryptedText);
+        assert decryptedText.equals(textInput.toString());
     }
 }
