ad2e123b182955b3f3566f086984d9f4fe2949a1
Francisco Rossi <francisco.rossi@opskins.com>
2019-08-21 15:05:54 -0300

    fixing algorithm

--- src/main/java/utils/Steganography.java
+++ src/main/java/utils/Steganography.java
@@ -15,6 +15,8 @@ import java.awt.image.WritableRaster;
 import java.awt.image.DataBufferByte;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 import javax.swing.JOptionPane;
@@ -22,32 +24,31 @@ import javax.swing.JOptionPane;
 public class Steganography {
 
     public void encode(String textToEncode, InputImage inputImage) throws IOException {
+        textToEncode = new StringBuilder(textToEncode).reverse().toString();
         byte[] inputImageMap = inputImage.getData();
         byte[] textBytes = textToEncode.getBytes(StandardCharsets.UTF_8);
 
         int imagePointer = 0;
         for (int i = 0; i < textBytes.length; i++) {
-            final int rLow = textBytes[i] >> 4;
-            final int rHigh = inputImageMap[imagePointer] & 0xF0;
-            inputImageMap[imagePointer] = (byte) (rLow + rHigh);
 
-            final int gLow = textBytes[i] & 0x0F;
-            final int gHigh = inputImageMap[imagePointer + 1] & 0xF0;
-            inputImageMap[imagePointer + 1] = (byte) (gLow + gHigh);
+            final byte b0L = (byte) (textBytes[i] >> 4);
+            final byte b0H = (byte) (inputImageMap[imagePointer] & 0b11110000);
+            inputImageMap[imagePointer] = (byte) (b0L + b0H);
 
-            //int bLow = (i == textBytes.length - 1) ? 0b00000001 : 0b00000000;
-            final int lastByte = inputImageMap[imagePointer + 2];
-            if (i == textBytes.length -1) {
-                inputImageMap[imagePointer + 2] = (byte) ( (lastByte << 1) | 0b00000001);
+            final byte b1L = (byte) (textBytes[i] & 0b00001111);
+            final byte b1H = (byte) (inputImageMap[imagePointer + 1] & 0b11110000);
+            inputImageMap[imagePointer + 1] = (byte) (b1L + b1H);
+
+            final byte lastByte = inputImageMap[imagePointer + 2];
+            if (i == textBytes.length - 1) {
+                inputImageMap[imagePointer + 2] = (byte) (lastByte | 0b00000001);
             } else {
-                inputImageMap[imagePointer + 2] = (byte) ( lastByte << 1);
+                inputImageMap[imagePointer + 2] = (byte) (lastByte & 0b11111110);
             }
 
-            if (inputImageMap[imagePointer + 1] % 2 == 0) {
-                imagePointer = imagePointer + (3 * 16);
-            } else {
-                imagePointer = imagePointer + (3);
-            }
+            imagePointer = imagePointer + decideJumpLength(inputImageMap[imagePointer],
+                    inputImageMap[imagePointer + 1]);
+
 
         }
 
@@ -61,22 +62,38 @@ public class Steganography {
         StringBuilder output = new StringBuilder();
         int imagePointer = 0;
         for (int i = 0; i < inputImageMap.length; i++) {
-            byte low = (byte) (inputImageMap[imagePointer] << 4);
-            byte high = (byte) (inputImageMap[imagePointer + 1] & 0x0F);
+            byte high = (byte) ((inputImageMap[imagePointer] & 0x0F) << 4);
+            byte low = (byte) (inputImageMap[imagePointer + 1] & 0x0F);
             byte[] c = new byte[1];
-            c[0] = (byte) (low + high);
+            c[0] = (byte) (high + low);
             output.append(new String(c, StandardCharsets.UTF_8));
+
             byte shouldStop = (byte) (inputImageMap[imagePointer + 2] & 0b00000001);
-            if (shouldStop == 1) {
+            if (shouldStop == 0b00000001) {
                 break;
             }
-            if (inputImageMap[imagePointer + 1] % 2 == 0) {
-                imagePointer = imagePointer + (3 * 16);
-            } else {
-                imagePointer = imagePointer + (3);
-            }
+
+            int jumpLength = decideJumpLength(inputImageMap[imagePointer],
+                    inputImageMap[imagePointer + 1]);
+            imagePointer = imagePointer + jumpLength;
+
         }
-        return output.toString();
+        return new StringBuilder(output).reverse().toString();
+    }
+
+    private int decideJumpLength(byte b0, byte b1) {
+        boolean isFirstByteEven = b0 % 2 == 0;
+        boolean isSecondByteEven = b1 % 2 == 0;
+
+        if (isFirstByteEven && isSecondByteEven) {
+            return 3 * 4;
+        } else if (isFirstByteEven) {
+            return 3;
+        } else if (isSecondByteEven) {
+            return 3 * 2;
+        } else {
+            return 3 * 3;
+        }
     }
 }
 
